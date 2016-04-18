1a4186661ccf9bee4ccf9cea06c17e59da471da3
josemalive <jose.maria.estrade@gmail.com>
2016-04-18 13:38:15 +0000

    Added eng and sp comments debugging chapter

--- 3.Debugging/2.2 Node Inspector/NodeInspector.js
+++ 3.Debugging/2.2 Node Inspector/NodeInspector.js
@@ -1,8 +1,8 @@
-Installation
-
+/* Eng: Install debug package */
+/* SP: Instalar package de Debug */
 $ npm install -g node-inspector
 
-Usage
-
+/* Eng: How to launch it */
+/* SP: Como lanzar el debugger */
 $ node-debug {myapp}.js //normal debug
 $ node-debug-brk {myapp}.js //first line
