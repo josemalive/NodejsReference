169cb9975437d3576a48197b00ff3618c7538206
josemalive <jose.maria.estrade@gmail.com>
2016-04-19 19:06:14 +0000

    Added more info about IDEs and Editors

--- 0.GettingStarted/0.3 IDE or Editor/IDEsOrEditor.js
+++ 0.GettingStarted/0.3 IDE or Editor/IDEsOrEditor.js
@@ -10,6 +10,26 @@
     IDEs:
         - Cloud9
             - http://c9.io 
+                * Real time collaboration
+                * GitHub, Bitbucket integration
+                * Preview in any browser
+                * Built in terminal
+                * Language tools
+                * Debugger
+                * Multiple languages support
+        - Nitrous
+            - https://www.nitrous.io
+                * Real time collaboration
+                * Fast file navigation
+                * SSL Data Encryption
+                * GitHub integration
+                * Not needed of setting up system
+                * Split Editing
+                * Backup & Redundancy
+                * Snapshots for preserving project environment
+                * Full SSH access
+                * Isolated environment, nothing is shared
+                
         - CodeAnywhere
             - https://codeanywhere.com/
         - Koding 
--- 1.NPM/1.0 Whats NPM/Readme.txt
+++ 1.NPM/1.0 Whats NPM/Readme.txt
@@ -1,7 +1,10 @@
 ENG: NPM stands for Node Package Manager and its a tool for install and manager
 third party modules. These modules are accesible from here: https://www.npmjs.com/
 Anyone can create a module and publish it into NPM repository.
+After Node is installed some "Core" modules are available, providing you a default API
+
 
 SP: NPM es el acronimo de Node Package manager y es una herramienta que permite instalar
 y gestionar modulos realizados por terceros, es accesible desde: https://www.npmjs.com/
 Cualquier desarrollador puede crear modulos y publicarlos en el repositorio NPM.
+Node viene con varios modulos "Core" disponibles para cualquier programador.
