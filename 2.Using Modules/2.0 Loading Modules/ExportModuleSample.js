9f95bf86cd76bd3b4ad42ef15c5a32d6986e4178
josemalive <jose.maria.estrade@gmail.com>
2016-05-13 06:23:50 +0000

    Fixed prototype name on section 2.0

--- 2.Using Modules/2.0 Loading Modules/ExportModuleSample.js
+++ 2.Using Modules/2.0 Loading Modules/ExportModuleSample.js
@@ -1,6 +1,6 @@
 //import the object "prototype"
 var MyObject=require('./ExportModule');
 //get a prototype object instance
-MyClass = new MyClass('param1','param2');
+MyObject = new MyObject('param1','param2');
 //Call Print Params
-MyClass.PrintParams();
+MyObject.PrintParams();
